e0ec40e731c2f54fe6e0d99404422a27d14d007a
jabc584 <ichlanddev@gmail.com>
2026-02-03 20:31:51 +0100

    Actualiza clase Producto

--- src/main/gallinaceas/Producto.java
+++ src/main/gallinaceas/Producto.java
@@ -1,36 +1,37 @@
+// Producto.java - Clase base abstracta mejorada
 package com.gallinaceas;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
+import java.time.temporal.ChronoUnit;
 
 public abstract class Producto {
-
     private final LocalDate fechaEnvasado;
     private final LocalDate fechaCaducidad;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-    protected Producto(int diasParaCaducar) {
+    
+    protected Producto(int diasCaducidad) {
         this.fechaEnvasado = LocalDate.now();
-        this.fechaCaducidad = this.fechaEnvasado.plusDays(diasParaCaducar);
+        this.fechaCaducidad = this.fechaEnvasado.plusDays(diasCaducidad);
     }
-
+    
+    public long diasHastaCaducidad() {
+        return ChronoUnit.DAYS.between(LocalDate.now(), fechaCaducidad);
+    }
+    
+    public boolean estaCaducado() {
+        return LocalDate.now().isAfter(fechaCaducidad);
+    }
+    
+    // Getters
     public String getFechaEnvasadoFormateada() {
         return fechaEnvasado.format(FORMATTER);
     }
-
+    
     public String getFechaCaducidadFormateada() {
         return fechaCaducidad.format(FORMATTER);
     }
-
-    public LocalDate getFechaEnvasado() {
-        return fechaEnvasado;
-    }
-
-    public LocalDate getFechaCaducidad() {
-        return fechaCaducidad;
-    }
-
+    
     @Override
     public abstract String toString();
 }
